af25cf95623e45779184e36a0e7cf625c2d322b0
Matthew Casperson <matthewcasperson@gmail.com>
2020-06-05 08:12:21 +1000

    Reverted lock file

--- beanstalk-bundle.ps1
+++ beanstalk-bundle.ps1
@@ -1,1 +1,1 @@
-Compress-Archive -Path app.rb, config.ru, Procfile, Gemfile, Gemfile.lock -DestinationPath beanstalk-app-bundle.zip -Force
+Compress-Archive -Path app.rb, config.ru, Procfile, Gemfile, Gemfile.lock, vendor -DestinationPath beanstalk-app-bundle.zip -Force
